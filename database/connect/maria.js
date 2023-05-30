5c3850b7ded045d41735037bc96c8aaed9bc6945
Yeon Kyung Ryu <celina16161616@gmail.com>
2023-05-30 15:19:54 +0900

    개발환경 세팅, express서버 연결

---
+++ database/connect/maria.js
@@ -0,0 +1,11 @@
+const maria = require('mysql');
+
+const conn = maria.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'yeonkyung',
+    password: '20201207',
+    database: 'bulletinboard'
+});
+
+module.exports = conn;
